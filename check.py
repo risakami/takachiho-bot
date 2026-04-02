e7a3474c5dcf506e280fe82993f0c3c34a59aa7a
risakami <risa.sui.0124@icloud.com>
2026-04-02 21:15:31 +0900

    check.py を更新

--- check.py
+++ check.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
-url ="https://eipro.jp/takachiho1/eventCalendars/index"
+url = "https://eipro.jp/takachiho1/eventCalendars/index"
 
 headers = {
     "User-Agent": "Mozilla/5.0"
@@ -10,7 +11,9 @@ headers = {
 res = requests.get(url, headers=headers)
 soup = BeautifulSoup(res.text, "html.parser")
 
-if "残" in soup.text:
-    print("残！")
+if "残1艇" in soup.text:
+    webhook = os.environ["DISCORD_WEBHOOK"]
+    requests.post(webhook, json={"content": "高千穂ボート空き出た！"})
+    print("通知送信")
 else:
-    print("なし")
+    print("空きなし")
